c6b0ee367a2493cc8973295ad6bfe4e5383a3b3c
kesy <92neco92@gmail.com>
2023-01-17 22:30:27 +0100

    Make functions for: Add new book object in the myLibrary array; Function print() for putting those object on page; Function for deleting book from page and array. And set up CSS rules of the page

--- script.js
+++ script.js
@@ -1,13 +1,65 @@
-let myLibrary = [];
+const form = document.forms[0];
+const shelves = document.querySelector(".shelves");
+const myLibrary = [
+  {
+    title: "Lord Of the Rings",
+    author: "Radoje Jezdic",
+    pages: 800,
+    read: true,
+  },
+  {
+    title: "Lord Of the Rings 2",
+    author: "Radoje Jezdic",
+    pages: 850,
+    read: true,
+  },
+];
 
-function book(title, author, pages, read) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  alert(
-    `This book name is ${this.title}, by ${this.author}, and it has ${this.pages} pages. And it's ${this.read} it`
-  );
 }
 
-function addBookToLibrary() {}
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); //sprecavanje ponovnog ucitavanja stranice
+
+  const newBook = new Book(
+    form.title.value,
+    form.author.value,
+    form.pages.value,
+    form.read.checked
+  );
+  myLibrary.push(newBook);
+  print();
+  form.reset();
+});
+
+function newElement(book) {
+  const el = document.createElement("div");
+  el.classList.add("book-card");
+  el.innerHTML = `<p>Book Name: ${book.title},</p><p>Written by: ${book.author},</p><p>Number of pages's: ${book.pages},</p><p>Readed: ${book.read}.</p>
+  <button>Delete BOOK</button>`;
+  return el;
+}
+
+function deleteBook(index) {
+  myLibrary.splice(index, 1);
+  print();
+}
+
+function print() {
+  shelves.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    shelves.appendChild(newElement(myLibrary[i]));
+  }
+  let btn = document.querySelectorAll(".book-card > button");
+  const buttons = [...btn];
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].setAttribute("value", i);
+    buttons[i].addEventListener("click", (e) => deleteBook(e.target.value));
+  }
+}
+
+print();
